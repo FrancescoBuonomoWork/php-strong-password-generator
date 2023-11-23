07ba3603a7d8e7b09369d89e9952b26064963f51
FrancescoBuonomo <buonomofrancescow@gmail.com>
2023-11-23 15:31:09 +0100

    add functions.php

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
 <?php
-$lenght_pass = $_GET['lenght_pass'] ?? 0;
-var_dump($lenght_pass);
-
-var_dump(generatePass($lenght_pass));
-function generatePass($lenght){
-    (int)$lenght;
-    $alphabet = 'abcdefghijkmnlopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
-    $new_pass = [];
-    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
-    for ($i=0; $i < $lenght;$i++){
-        $n = rand(0, $alphaLength);
-        $new_pass[] = $alphabet[$n];
-    };
-    return implode($new_pass); //turn the array into a string
-
-};
+include './functions.php';
 
 ?>
 <!DOCTYPE html>
